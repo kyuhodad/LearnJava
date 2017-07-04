cbbf19015e6ccdb2e83fe46e6d1033e148d3c496
kyuhodad <kyuhodad@gmail.com>
2017-07-04 00:02:15 -0400

    Add HelloJava.

---
+++ src/HelloJava.java
@@ -0,0 +1,23 @@
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!!");
+		
+		// Primitive variable types
+		
+		
+		// Array
+		
+		
+		// Operators and expression
+		
+		
+		// Condition and flow controls
+		
+		
+		// Loops
+		
+				
+		// Assignment
+	}
+}
